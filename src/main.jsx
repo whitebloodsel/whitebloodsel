2e34ef17be9f0c520171d8dad1e04f2590913254
gisella <gisella.j05@gmail.com>
2026-02-17 18:10:14 +0700

    add polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,14 @@
+// Polyfill for URL.parse (Fixes Safari < 18 crash)
+if (typeof window !== "undefined" && !URL.parse) {
+  URL.parse = (url, base) => {
+    try {
+      return new URL(url, base);
+    } catch {
+      return null;
+    }
+  };
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
